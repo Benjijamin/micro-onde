dd87e2392395acdb0e2bdd495f6374e864815376
Beru <sberube1999@gmail.com>
2026-05-05 21:10:43 -0400

    main menu base

---
+++ Assets/Scripts/UI/Menus/MainMenu.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Button levelSelectionButton;
+    [SerializeField] private Button optionsButton;
+    [SerializeField] private Button quitButton;
+
+    [SerializeField] private GameObject startMenu;
+    [SerializeField] private GameObject levelSelectionMenu;
+    [SerializeField] private GameObject OptionsMenu;
+
+    public static MainMenu instance { get; private set; }
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        levelSelectionButton.onClick.AddListener(GoToLevelSelection);
+        quitButton.onClick.AddListener(Application.Quit);
+        
+    }
+
+    public void GoToLevelSelection()
+    {
+        startMenu.SetActive(false);
+        //OptionsMenu.SetActive(false);
+
+        levelSelectionMenu.SetActive(true);
+    }
+
+    public void GoToStartMenu()
+    {
+        levelSelectionMenu.SetActive(false);
+        //OptionsMenu.SetActive(false);
+
+        startMenu.SetActive(true);
+    }
+
+    public void LoadLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
