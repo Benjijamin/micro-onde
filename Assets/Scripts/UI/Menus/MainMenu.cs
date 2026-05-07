2a44b12f7689d624cc6b42c53ddf7c1e13c8493f
Vincent Paquet <paquetvincent045@gmail.com>
2026-05-06 23:48:42 -0400

    warning

--- Assets/Scripts/UI/Menus/MainMenu.cs
+++ Assets/Scripts/UI/Menus/MainMenu.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,6 +11,9 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private GameObject startMenu;
     [SerializeField] private GameObject levelSelectionMenu;
     [SerializeField] private GameObject OptionsMenu;
+    [SerializeField] private CanvasGroup warningScreen;
+    [SerializeField] private float warningDuration;
+    [SerializeField] private float warningFadeOutTime;
 
     public static MainMenu instance { get; private set; }
     private void Awake()
@@ -28,7 +32,20 @@ public class MainMenu : MonoBehaviour
     {
         levelSelectionButton.onClick.AddListener(GoToLevelSelection);
         quitButton.onClick.AddListener(Application.Quit);
-        
+        StartCoroutine(FadeOutWarning());
+    }
+
+    private IEnumerator FadeOutWarning()
+    {
+        yield return new WaitForSeconds(warningDuration);
+        float t = 0;
+        while (warningScreen.alpha > 0)
+        {
+            yield return null;
+            t += Time.deltaTime;
+            warningScreen.alpha = Mathf.Lerp(1, 0, t / warningFadeOutTime);
+        }
+        warningScreen.gameObject.SetActive(false);
     }
 
     public void GoToLevelSelection()
